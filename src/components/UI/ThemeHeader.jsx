ba95e7f0bbdf5e0139584c2719a24d5549d5f1a4
moskalenkoyegor <yegormoss@gmail.com>
2022-07-17 02:04:42 +0300

    removed redundant code

--- src/components/UI/ThemeHeader.jsx
+++ src/components/UI/ThemeHeader.jsx
@@ -1,35 +1,18 @@
 import Loader from "./loader/Loader";
 
 const ThemeHeader = ({uahToCurrency}) => {
-    if(uahToCurrency.EUR === 0 || uahToCurrency.USD === 0){
-        return (
-            <div className="header">
-                <div className="header__content">
-                    <div className='exchange__rates'>
-                        <div style={{display: 'flex'}} className='exchange__rate__1 exchange__blocks'>
-                            <Loader />
-                            <div style={{marginLeft: '15px'}}>UAH/USD</div>
-                        </div>
-                        <div style={{display: 'flex'}} className='exchange__rate__2 exchange__blocks'>
-                            <Loader />
-                            <div style={{marginLeft: '15px'}}>UAH/EUR</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
     return (
         <div className="header">
             <div className="header__content">
                 <div className='exchange__rates'>
-                    <div className='exchange__rate__1 exchange__blocks'>
-                        {uahToCurrency.USD} - UAH/USD
-                    </div>
-                    <div className='exchange__rate__2 exchange__blocks'>
-                        {uahToCurrency.EUR} - UAH/EUR
-                    </div>
+                    {uahToCurrency.USD === 0
+                        ? <div style={{display: 'flex'}} className='exchange__rate__1 exchange__blocks'><Loader /><div style={{marginLeft: '15px'}}>UAH/USD</div></div>
+                        : <div className='exchange__rate__2 exchange__blocks'>{uahToCurrency.USD} - UAH/USD</div>
+                    }
+                    {uahToCurrency.EUR === 0
+                        ? <div style={{display: 'flex'}} className='exchange__rate__1 exchange__blocks'><Loader /><div style={{marginLeft: '15px'}}>UAH/USD</div></div>
+                        : <div className='exchange__rate__2 exchange__blocks'>{uahToCurrency.EUR} - UAH/EUR</div>
+                    }
                 </div>
             </div>
         </div>
