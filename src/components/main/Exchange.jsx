83f3d7b8759cc2605b4bb2bb9d8648fc26f4ed79
moskalenkoyegor <yegormoss@gmail.com>
2022-07-20 18:53:01 +0300

    changed function getResult

--- src/components/main/Exchange.jsx
+++ src/components/main/Exchange.jsx
@@ -1,26 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import ThemeInput from "../UI/input/ThemeInput";
 import CurrencySelect1 from "./currencySelects/CurrencySelect1";
 import CurrencySelect2 from "./currencySelects/CurrencySelect2";
-import ThemeInput from "../UI/input/ThemeInput";
+import Loader from "../UI/loader/Loader";
+
 
 const Exchange = ({result, setResult, requestOptions}) => {
+
     const currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL"]
 
+    const [amount, setAmount] = useState('')
     const [currency1, setCurrency1] = useState('')
     const [currency2, setCurrency2] = useState('')
-
-    const [amount, setAmount] = useState('')
+    const [loading, setLoading] = useState(null)
 
     const getResult = () => {
-        if(amount !== '' && currency1 !== '' && currency2 !== ''){
+        setLoading(loading => loading = true)
+        if(currency1 !== '' || currency2 !== '' || amount > 0){
             fetch(`https://api.apilayer.com/fixer/convert?to=${currency2}&from=${currency1}&amount=${amount}`, requestOptions)
                 .then(responseJson => responseJson.json())
-                .then(response => setResult(response.result))
+                .then(response => setResult(result => result = response.result))
+                .then(() => setLoading(loading => loading = false))
                 .catch(e => console.log('Error-1', e))
         } else {
             console.error('Fields cannot be empty')
         }
     }
+
+    const isInitialMount = useRef(true);
+
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            getResult()
+        }
+    }, [currency1, currency2])
+
+
 
     return (
         <div className='main'>
@@ -37,7 +54,7 @@ const Exchange = ({result, setResult, requestOptions}) => {
                             <p className='exchange__p'>From: </p>
                             <CurrencySelect1
                                 value={currency1}
-                                onChange={selectedCurrency => setCurrency1(selectedCurrency)}
+                                onChange={selectedCurrency => setCurrency1(currency1 => currency1 = selectedCurrency)}
                                 defaultValue='Choose'
                                 options={currencies}
                             />
@@ -46,21 +63,31 @@ const Exchange = ({result, setResult, requestOptions}) => {
                             <p className='exchange__p'>To: </p>
                             <CurrencySelect2
                                 value={currency2}
-                                onChange={selectedCurrency => setCurrency2(selectedCurrency)}
+                                onChange={selectedCurrency => setCurrency2(currency2 => currency2 = selectedCurrency)}
                                 defaultValue='Choose'
                                 options={currencies}
                             />
                         </div>
                     </div>
                 </div>
-                <div className='get__result'>
+                {loading &&
+                 <Loader />
+                }
+                {loading === false &&
                     <div className='exchange__result'>
-                        <div className='exchange__result__content'>
-                            {result}
-                        </div>
+                        {currency1 !== '' && currency2 !== '' &&
+                            <div className='exchange__result__content'>
+                                {amount > 0 ? result : <div style={{textAlign: 'center', color: 'red'}}>Error</div>}
+                            </div>
+                        }
+                        {currency1 === '' || currency2 === ''
+                            ?   <div className='exchange__result__content'>
+                                    <div style={{textAlign: 'center'}}>You need to choose 2 currencies</div>
+                                </div>
+                            : <div></div>
+                        }
                     </div>
-                    <button className='btn' style={{marginLeft: '20px'}} onClick={getResult}>Get result</button>
-                </div>
+                }
             </div>
         </div>
     );
